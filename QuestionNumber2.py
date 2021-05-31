113f24abde4a44edebb8ceea5c471f474946df2e
ShashankaDhungana <shashanka.dd@gmail.com>
2021-05-31 12:00:16 +0545

    initial commit

---
+++ QuestionNumber2.py
@@ -0,0 +1,10 @@
+'''Write a program that reads the length of the base and the height of a right-angled triangle and prints the area.
+Every number is given on a separate line'''
+
+l = int(input('enter length:'))
+h = int(input('enter height:'))
+area = (l * h) / 2
+print(area)
+print(f'The area of right angled triangle is : {area}')
+
+
---
+++ Questionnumber1.py
@@ -0,0 +1,7 @@
+''''''
+num1 = int(input('enter a number:'))
+num2 = int(input('enter a number:'))
+num3 = int(input('enter a number:'))
+SUM = num1 + num2 + num3
+print(SUM)
+print(f'the sum of three numbers is: {SUM}')
